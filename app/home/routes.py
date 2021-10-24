d42e543b6532ea976583519655e08451a8effd89
Marlyne Hakizimana <marlhaki12@gmail.com>
2021-10-24 04:05:26 +0000

    added dashboard

--- app/home/routes.py
+++ app/home/routes.py
@@ -8,6 +8,46 @@ from flask import render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
+from google.cloud import bigquery
+import json
+import pandas as pd
+
+
+def waste_table():
+    client = bigquery.Client()
+    demand = (
+            'SELECT * FROM `food-waste-329921.Foodwaste.Demand`;'
+        )
+    inventory = (
+        'SELECT * FROM `food-waste-329921.Foodwaste.Inventory`;'
+    )
+    predicted = (
+            'SELECT * FROM `food-waste-329921.Foodwaste.Predicted`;'
+        )
+    recipe = (
+            'SELECT * FROM `food-waste-329921.Foodwaste.Recipe`;'
+        )
+
+    Demand = client.query(demand).to_dataframe().sort_values(by=['Date', 'Recipe'])
+    Inventory = client.query(inventory).to_dataframe()
+    Predicted = client.query(predicted).to_dataframe().sort_values(by=['Date', 'ITEM'])
+    Recipe = client.query(recipe).to_dataframe()
+    diff=Predicted['Predicted_Servings']-Demand['Quantity']
+    leftoverdf=Demand.copy()
+    leftoverdf['Quantity']=diff
+    qtyleft_dfs=[]
+
+    for i in range(len(leftoverdf)):        
+        recipe_i = leftoverdf.Recipe[i]    
+        recip=Recipe[Recipe['Recipe_Name']==recipe_i]
+        divid=leftoverdf['Quantity'][i]/recip['Servings'].unique()[0]
+        df_i = pd.DataFrame(data={"ingredient":recip['Ingredients'],"qtywasted":divid})
+        
+        qtyleft_dfs.append(df_i)
+    dfwasted = pd.concat(qtyleft_dfs,ignore_index=True).sort_values(by=['ingredient'])
+    dfwasted1=dfwasted.groupby('ingredient')['qtywasted'].sum()
+    return dfwasted1
+
 
 @blueprint.route('/index')
 @login_required
@@ -23,10 +63,17 @@ def table():
         #     template += '.html'
 
         # Detect the current page
+        print("here")
         segment = get_segment( request )
-
-        # # Serve the file (if exists) from app/templates/FILE.html
-        return render_template('table.html', segment=segment )
+        table = waste_table()
+  
+    # parsing the DataFrame in json format.
+        json_records = table.reset_index().to_json(orient ='records')
+        data = []
+        data = json.loads(json_records)
+        print(data)
+        # context = {'d': data}
+        return render_template('table.html', table=data)
 
     except TemplateNotFound:
         return render_template('page-404.html'), 404
